93e6ee60799d4d787d5fac1c725b737f4c68d99e
张磊 <kmdjs@qq.com>
2016-11-18 07:59:20 +0800

    replace empty functions width single noop. thanks for kurtextrem's advice.

--- alloy_finger.js
+++ alloy_finger.js
@@ -2,7 +2,7 @@
  * By dntzhang
  * Github: https://github.com/AlloyTeam/AlloyFinger
  */
-;(function() {
+; (function () {
     function getLen(v) {
         return Math.sqrt(v.x * v.x + v.y * v.y);
     }
@@ -36,26 +36,29 @@
         el.addEventListener("touchstart", this.start.bind(this), false);
         el.addEventListener("touchmove", this.move.bind(this), false);
         el.addEventListener("touchend", this.end.bind(this), false);
-        el.addEventListener("touchcancel",this.cancel.bind(this),false);
+        el.addEventListener("touchcancel", this.cancel.bind(this), false);
 
         this.preV = { x: null, y: null };
         this.pinchStartLen = null;
         this.scale = 1;
         this.isDoubleTap = false;
-        this.rotate = option.rotate || function () { };
-        this.touchStart = option.touchStart || function () { };
-        this.multipointStart = option.multipointStart || function () { };
-        this.multipointEnd=option.multipointEnd||function(){};
-        this.pinch = option.pinch || function () { };
-        this.swipe = option.swipe || function () { };
-        this.tap = option.tap || function () { };
-        this.doubleTap = option.doubleTap || function () { };
-        this.longTap = option.longTap || function () { };
-        this.singleTap = option.singleTap || function () { };
-        this.pressMove = option.pressMove || function () { };
-        this.touchMove = option.touchMove || function () { };
-        this.touchEnd = option.touchEnd || function () { };
-        this.touchCancel = option.touchCancel || function () { };
+
+        var noop = function () { };
+
+        this.rotate = option.rotate || noop;
+        this.touchStart = option.touchStart || noop;
+        this.multipointStart = option.multipointStart || noop;
+        this.multipointEnd = option.multipointEnd || noop;
+        this.pinch = option.pinch || noop;
+        this.swipe = option.swipe || noop;
+        this.tap = option.tap || noop;
+        this.doubleTap = option.doubleTap || noop;
+        this.longTap = option.longTap || noop;
+        this.singleTap = option.singleTap || noop;
+        this.pressMove = option.pressMove || noop;
+        this.touchMove = option.touchMove || noop;
+        this.touchEnd = option.touchEnd || noop;
+        this.touchCancel = option.touchCancel || noop;
 
         this.delta = null;
         this.last = null;
@@ -63,24 +66,24 @@
         this.tapTimeout = null;
         this.touchTimeout = null;
         this.longTapTimeout = null;
-        this.swipeTimeout=null;
+        this.swipeTimeout = null;
         this.x1 = this.x2 = this.y1 = this.y2 = null;
-        this.preTapPosition={x:null,y:null};
+        this.preTapPosition = { x: null, y: null };
     };
 
     AlloyFinger.prototype = {
         start: function (evt) {
-            if(!evt.touches)return;
+            if (!evt.touches) return;
             this.now = Date.now();
             this.x1 = evt.touches[0].pageX;
             this.y1 = evt.touches[0].pageY;
             this.delta = this.now - (this.last || this.now);
             this.touchStart(evt);
-            if(this.preTapPosition.x!==null){
-                this.isDoubleTap = (this.delta > 0 && this.delta <= 250&&Math.abs(this.preTapPosition.x-this.x1)<30&&Math.abs(this.preTapPosition.y-this.y1)<30);
+            if (this.preTapPosition.x !== null) {
+                this.isDoubleTap = (this.delta > 0 && this.delta <= 250 && Math.abs(this.preTapPosition.x - this.x1) < 30 && Math.abs(this.preTapPosition.y - this.y1) < 30);
             }
-            this.preTapPosition.x=this.x1;
-            this.preTapPosition.y=this.y1;
+            this.preTapPosition.x = this.x1;
+            this.preTapPosition.y = this.y1;
             this.last = this.now;
             var preV = this.preV,
                 len = evt.touches.length;
@@ -92,17 +95,17 @@
                 this.pinchStartLen = getLen(preV);
                 this.multipointStart(evt);
             }
-            this.longTapTimeout = setTimeout(function(){
+            this.longTapTimeout = setTimeout(function () {
                 this.longTap(evt);
             }.bind(this), 750);
         },
         move: function (evt) {
-            if(!evt.touches)return;
+            if (!evt.touches) return;
             var preV = this.preV,
                 len = evt.touches.length,
                 currentX = evt.touches[0].pageX,
                 currentY = evt.touches[0].pageY;
-            this.isDoubleTap=false;
+            this.isDoubleTap = false;
             if (len > 1) {
                 var v = { x: evt.touches[1].pageX - currentX, y: evt.touches[1].pageY - currentY };
 
@@ -122,7 +125,7 @@
                     evt.deltaX = currentX - this.x2;
                     evt.deltaY = currentY - this.y2;
 
-                }else{
+                } else {
                     evt.deltaX = 0;
                     evt.deltaY = 0;
                 }
@@ -140,10 +143,10 @@
             }
         },
         end: function (evt) {
-            if(!evt.changedTouches)return;
+            if (!evt.changedTouches) return;
             this._cancelLongTap();
             var self = this;
-            if( evt.touches.length<2){
+            if (evt.touches.length < 2) {
                 this.multipointEnd(evt);
             }
             this.touchEnd(evt);
@@ -163,10 +166,10 @@
                         self.doubleTap(evt);
                         clearTimeout(self.touchTimeout);
                         self.isDoubleTap = false;
-                    }else{
-                        self.touchTimeout=setTimeout(function(){
+                    } else {
+                        self.touchTimeout = setTimeout(function () {
                             self.singleTap(evt);
-                        },250);
+                        }, 250);
                     }
                 }, 0)
             }
@@ -177,7 +180,7 @@
             this.pinchStartLen = null;
             this.x1 = this.x2 = this.y1 = this.y2 = null;
         },
-        cancel:function(evt){
+        cancel: function (evt) {
             clearTimeout(this.touchTimeout);
             clearTimeout(this.tapTimeout);
             clearTimeout(this.longTapTimeout);
@@ -196,7 +199,7 @@
 
     if (typeof module !== 'undefined' && typeof exports === 'object') {
         module.exports = AlloyFinger;
-    }else {
+    } else {
         window.AlloyFinger = AlloyFinger;
     }
 })();
