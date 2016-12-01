9f5fb070fb19dc3f9bee051921433302734771f2
june_01 <juneandgreen@gmail.com>
2016-12-01 13:13:42 +0800

    update readme

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ npm install alloyfinger
 # Usage
 
 ```js
-new AlloyFinger(element, {
+var af = new AlloyFinger(element, {
     touchStart: function () { },
     touchMove: function () { },
     touchEnd:  function () { },
@@ -36,6 +36,13 @@ new AlloyFinger(element, {
         console.log("swipe" + evt.direction);
     }
 });
+
+/**
+ * this method can also add the event handler
+ */
+af.on('tap', function() {});
+af.on('touchStart', function() {});
+
 ```
 
 ### React Version:
@@ -62,25 +69,74 @@ render() {
 ### Vue1 & Vue2 Version:
 
 ```html
-<alloy-finger
-    v-on:tap="onTap"
-    v-on:multipoint-start="onMultipointStart"
-    v-on:long-tap="onLongTap"
-    v-on:swipe="onSwipe"
-    v-on:pinch="onPinch"
-    v-on:rotate="onRotate"
-    v-on:press-move="onPressMove"
-    v-on:multipoint-end="onMultipointEnd"
-    v-on:double-tap="onDoubleTap"
+<div id="cnt">
+    <div
+        v-finger:tap="tap"
+        v-finger:multipoint-start="multipointStart"
+        v-finger:long-tap="longTap"
+        v-finger:swipe="swipe"
+        v-finger:pinch="pinch"
+        v-finger:rotate="rotate"
+        v-finger:press-move="pressMove"
+        v-finger:multipoint-end="multipointEnd"
+        v-finger:double-tap="doubleTap"
+        v-finger:single-tap="singleTap"
 
-    v-on:touch-start="onTouchStart"
-    v-on:touch-move="onTouchMove"
-    v-on:touch-end="onTouchEnd"
-    v-on:touch-cancel="onTouchCancel"
->
-    <div>the element that you want to bind event</div>
-</alloy-finger>
+        v-finger:touch-start="touchStart"
+        v-finger:touch-move="touchMove"
+        v-finger:touch-end="touchEnd"
+        v-finger:touch-cancel="touchCancel"
+    >
+        <div>the element that you want to bind event</div>
+    </div>
+</div>
+
+<!-- vue.js -->
+<script type="text/javascript" src="./vue.min.js"></script>
+<!-- need include the original alloy_finger.js -->
+<script type="text/javascript" src="./alloy_finger.js"></script>
+<!-- AlloyFinger's plugin -->
+<script type="text/javascript" src="./alloy_finger.vue.js"></script>
 ```
+
+```js
+Vue.use(AlloyFingerVue); // use AlloyFinger's plugin
+
+var h = new Vue({
+    el: '#cnt',
+    methods: {
+        tap: function() { console.log('onTap'); },
+        multipointStart: function() { console.log('onMultipointStart'); },
+        longTap: function() { console.log('onLongTap'); },
+        swipe: function(evt) {
+            console.log("swipe" + evt.direction);
+            console.log('onSwipe');
+        },
+        pinch: function(evt) { 
+            console.log(evt.scale);
+            console.log('onPinch'); 
+        },
+        rotate: function(evt) {
+            console.log(evt.angle);
+            console.log('onRotate'); 
+        },
+        pressMove: function(evt) { 
+            console.log(evt.deltaX);
+            console.log(evt.deltaY);
+            console.log('onPressMove'); 
+        },
+        multipointEnd: function() { console.log('onMultipointEnd'); },
+        doubleTap: function() { console.log('onDoubleTap'); },
+        singleTap: function () { console.log('onSingleTap'); },
+
+        touchStart: function() { console.log('onTouchStart'); },
+        touchMove: function() { console.log('onTouchMove'); },
+        touchEnd: function() { console.log('onTouchEnd'); },
+        touchCancel: function() { console.log('onTouchCancel'); }
+    }
+});
+```
+
 # Many thanks to 
 [transformjs](http://alloyteam.github.io/AlloyTouch/transformjs/)
 
--- vue/alloy_finger.vue.js
+++ vue/alloy_finger.vue.js
@@ -1,189 +1,83 @@
 /* AlloyFinger v0.1.0 for Vue
- * By dntzhang and june01
+ * By june01
  * Github: https://github.com/AlloyTeam/AlloyFinger
  */
 
-// definition
-var AlloyFinger = Vue.extend({
-	template: '<div v-on:touchstart="_handleTouchStart" v-on:touchmove="_handleTouchMove" v-on:touchcancel="_handleTouchCancel" v-on:touchend="_handleTouchEnd"><slot></slot></div>',
-	data: function() {
-		return {
-			preV: { x: null, y: null },
-      pinchStartLen: null,
-      scale: 1,
-      isDoubleTap: false,
-      delta: null,
-      last: null,
-      now: null,
-      tapTimeout: null,
-      longTapTimeout: null,
-      swipeTimeout: null,
-      x1: null,
-      x2: null,
-      y1: null,
-      y2: null,
-      preTapPosition: { x: null, y: null },
-		}
-	},
-	methods: {
-		getLen: function(v) {
-      return Math.sqrt(v.x * v.x + v.y * v.y);
-    },
+; (function() {
 
-   	dot: function(v1, v2) {
-      return v1.x * v2.x + v1.y * v2.y;
-    },
+  var AlloyFingerPlugin = {
+    install: function(Vue, options) {
+      options = options || {};
+      var AlloyFinger = window.AlloyFinger || options.AlloyFinger;
 
-    getAngle: function(v1, v2) {
-      var mr = getLen(v1) * getLen(v2);
-      if (mr === 0) return 0;
-      var r = dot(v1, v2) / mr;
-      if (r > 1) r = 1;
-      return Math.acos(r);
-    },
-
-    cross: function(v1, v2) {
-      return v1.x * v2.y - v2.x * v1.y;
-    },
-
-    getRotateAngle: function(v1, v2) {
-      var angle = getAngle(v1, v2);
-      if (cross(v1, v2) > 0) {
-        angle *= -1;
+      if(!AlloyFinger) {
+        throw new Error('you need include the AlloyFinger!');
       }
 
-      return angle * 180 / Math.PI;
-    },
+      var EVENTMAP = {
+        'touch-start': 'touchStart',
+        'touch-move': 'touchMove',
+        'touch-end': 'touchEnd',
+        'touch-cancel': 'touchCancel',
+        'multipoint-start': 'multipointStart',
+        'multipoint-end': 'multipointEnd',
+        'tap': 'tap',
+        'double-tap': 'doubleTap',
+        'long-tap': 'longTap',
+        'single-tap': 'singleTap',
+        'rotate': 'rotate',
+        'pinch': 'pinch',
+        'press-move': 'pressMove',
+        'swipe': 'swipe'
+      };
 
-    _handleTouchStart: function(evt) {
-      var emit = this.$dispatch || this.$emit;
+      var CACHE = [];
 
-      emit.call(this, 'touch-start', evt);
+      // definition
+      Vue.directive('finger', function(elem, binding) {
+        if(Vue.version.substr(0,1) <= 1) {
+          // vue1.xx
+          binding = {
+            value: elem,
+            arg: this.arg
+          };
 
-			this.now = Date.now();
-			this.x1 = evt.touches[0].pageX;
-			this.y1 = evt.touches[0].pageY;
-			this.delta = this.now - (this.last || this.now);
-			if(this.preTapPosition.x!==null){
-				this.isDoubleTap = (this.delta > 0 && this.delta <= 250&&Math.abs(this.preTapPosition.x-this.x1)<30&&Math.abs(this.preTapPosition.y-this.y1)<30);
-			}
-			this.preTapPosition.x=this.x1;
-			this.preTapPosition.y=this.y1;
-			this.last = this.now;
-     	var preV = this.preV,
-      		len = evt.touches.length;
-     	if (len > 1) {
-				var v = { x: evt.touches[1].pageX - this.x1, y: evt.touches[1].pageY - this.y1 };
-				preV.x = v.x;
-				preV.y = v.y;
-				this.pinchStartLen = getLen(preV);
-				emit.call(this, 'multipoint-start', evt);
-     	}
-     	this.longTapTimeout = setTimeout(function(){
-        emit.call(this, 'long-tap', evt);
-     	}.bind(this), 750);
-    },
-
-    _handleTouchMove: function(evt){
-      var emit = this.$dispatch || this.$emit;
-
-      emit.call(this, 'touch-move', evt);
-
-      var preV = this.preV,
-	        len = evt.touches.length,
-	        currentX = evt.touches[0].pageX,
-	        currentY = evt.touches[0].pageY;
-      this.isDoubleTap=false;
-      if (len > 1) {
-        var v = { x: evt.touches[1].pageX - currentX, y: evt.touches[1].pageY - currentY };
-
-        if (preV.x !== null) {
-          if (this.pinchStartLen > 0) {
-            evt.scale = getLen(v) / this.pinchStartLen;
-            emit.call(this, 'pinch', evt);
-          }
-
-          evt.angle = getRotateAngle(v, preV);
-          emit.call(this, 'rotate', evt);
+          elem = this.el;
         }
-        preV.x = v.x;
-        preV.y = v.y;
-      } else {
-        if (this.x2 !== null) {
-          evt.deltaX = currentX - this.x2;
-          evt.deltaY = currentY - this.y2;
-        }else{
-          evt.deltaX = 0;
-          evt.deltaY = 0;
-        }
-        emit.call(this, 'press-move', evt);
-      }
-      this._cancelLongTap();
-      this.x2 = currentX;
-      this.y2 = currentY;
-      if(len > 1) {
-        evt.preventDefault();
-      }
-    },
 
-    _handleTouchCancel: function(){
-      emit.call(this, 'touch-cancel', evt);
+        var func = binding.value || function() {};
+        var eventName = binding.arg;
 
-      clearInterval(this.tapTimeout);
-      clearInterval(this.longTapTimeout);
-      clearInterval(this.swipeTimeout);
-    },
+        eventName = EVENTMAP[eventName];
 
-    _handleTouchEnd: function(evt){
-      var emit = this.$dispatch || this.$emit;
-
-      emit.call(this, 'touch-end', evt);
-
-      this._cancelLongTap();
-      var self = this;
-      if( evt.touches.length<2){
-        emit.call(this, 'multipoint-end', evt);
-      }
-
-      if ((this.x2 && Math.abs(this.x1 - this.x2) > 30) ||
-        (this.y2 && Math.abs(this.preV.y - this.y2) > 30)) {
-        evt.direction = this._swipeDirection(this.x1, this.x2, this.y1, this.y2);
-        this.swipeTimeout = setTimeout(function () {
-            emit.call(self, 'swipe', evt);
-        }, 0)
-      } else {
-        this.tapTimeout = setTimeout(function () {
-          emit.call(self, 'tap', evt);
-          if (self.isDoubleTap) {
-            emit.call(self, 'double-tap', evt);
-            self.isDoubleTap = false;
+        var cacheObj;
+        for(var i=0,len=CACHE.length; i<len; i++) {
+          if(CACHE[i].elem === elem) {
+            cacheObj = CACHE[i];
+            break;
           }
-        }, 0)
-      }
+        }
 
-      this.preV.x = 0;
-      this.preV.y = 0;
-      this.scale = 1;
-      this.pinchStartLen = null;
-      this.x1 = this.x2 = this.y1 = this.y2 = null;
-    },
+        if(cacheObj && cacheObj.alloyFinger) {
+          cacheObj.alloyFinger.on(eventName, func);
+        } else {
+          var options = {};
+          options[eventName] = func;
 
-    _cancelLongTap: function() {
-      clearTimeout(this.longTapTimeout);
-    },
-
-    _swipeDirection: function(x1, x2, y1, y2) {
-      return Math.abs(x1 - x2) >= Math.abs(y1 - y2) ? (x1 - x2 > 0 ? 'Left' : 'Right') : (y1 - y2 > 0 ? 'Up' : 'Down')
+          CACHE.push({
+            elem: elem,
+            alloyFinger: new AlloyFinger(elem, options)
+          });
+        }
+      });
     }
-	}
-});
+  }
 
-// register
-Vue.component('alloy-finger', AlloyFinger);
+  // export
+  if(typeof module !== 'undefined' && typeof exports === 'object') {
+    module.exports = AlloyFingerPlugin;
+  } else {
+    window.AlloyFingerVue = AlloyFingerPlugin;
+  }
 
-// export
-if(typeof module !== 'undefined' && typeof exports === 'object') {
-  module.exports = AlloyFinger;
-} else {
-  window.AlloyFinger = AlloyFinger;
-}
+})();
