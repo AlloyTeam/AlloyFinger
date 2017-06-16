bcab6d8996b2730fa2453504a2ecab061aa64a69
maiff <864306867@qq.com>
2017-06-16 16:36:10 +0800

    add 'this'

--- react/react-alloy_finger.jsx
+++ react/react-alloy_finger.jsx
@@ -81,7 +81,7 @@ export default class AlloyFinger extends React.Component {
            var v = { x: evt.touches[1].pageX - this.x1, y: evt.touches[1].pageY - this.y1 };
            preV.x = v.x;
            preV.y = v.y;
-           this.pinchStartLen = getLen(preV);
+           this.pinchStartLen = this.getLen(preV);
            this._emitEvent('onMultipointStart', evt);
        }
        this.longTapTimeout = setTimeout(function(){
