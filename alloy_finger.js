5fba026c818b0f909138af30b9f16ec759871dc3
jiabin.chen <jiabin.chen@weimob.com>
2018-02-26 14:30:16 +0800

    fix singleTap problem

--- alloy_finger.js
+++ alloy_finger.js
@@ -127,6 +127,7 @@
             this.touchStart.dispatch(evt, this.element);
             if (this.preTapPosition.x !== null) {
                 this.isDoubleTap = (this.delta > 0 && this.delta <= 250 && Math.abs(this.preTapPosition.x - this.x1) < 30 && Math.abs(this.preTapPosition.y - this.y1) < 30);
+                if (this.isDoubleTap) clearTimeout(self.singleTapTimeout);
             }
             this.preTapPosition.x = this.x1;
             this.preTapPosition.y = this.y1;
