ed11c3f9c9982538ca058ba2634af467062007d7
zsusyt <zsusyt@zsusytdeMacBook-Pro.local>
2018-03-22 08:07:33 +0800

    change let to var

--- alloy_finger.js
+++ alloy_finger.js
@@ -190,7 +190,7 @@
 
                     //move事件中添加对当前触摸点到初始触摸点的判断，
                     //如果曾经大于过某个距离(比如10),就认为是移动到某个地方又移回来，应该不再触发tap事件才对。
-                    let movedX = Math.abs(this.x1 - this.x2),
+                    var movedX = Math.abs(this.x1 - this.x2),
                         movedY = Math.abs(this.y1 - this.y2);
 
                     if(movedX > 10 || movedY > 10){
